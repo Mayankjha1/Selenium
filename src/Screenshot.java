3060d36c114da965f38cabba477fadcd01a30fd4
Mayank <Mayankjha237@gmail.com>
2023-11-08 12:22:01 +0530

    Committed

--- src/Screenshot.java
+++ src/Screenshot.java
@@ -1,9 +1,8 @@
+// This is the code/Script to take Screenshots
+
 import java.io.File;
-import java.nio.file.Files;
-import java.util.logging.FileHandler;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,8 +11,6 @@ public class Screenshot {
         System.out.println("Hello, World!");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
-        // File SrcScreenshot = driver.findElement(By.id("input")).getScreenshotAs(OutputType.FILE);
-        // FileHandler.copyFile(SrcScreenshot,new File(System.getProperty("user.dir")+"\\Screenshot.png"));
         File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
         com.google.common.io.Files.copy(scrFile, new File("Screenshot1.png"));       
         driver.quit();   
